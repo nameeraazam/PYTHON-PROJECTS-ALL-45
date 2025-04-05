3bc0ea7b3e7ca5a6dc611db7e6d37897d497ddb1
Syeda Nameera Azam <163463330+nameeraazam@users.noreply.github.com>
2025-04-06 00:53:00 +0500

    Update 01-add two numbers.py

--- python github/ASSIGNMENT 00 to 05/00-intro python/01 add two numbers/01-add two numbers.py
+++ python github/ASSIGNMENT 00 to 05/00-intro python/01 add two numbers/01-add two numbers.py
@@ -1,47 +1,3 @@
-#01 add two numbers.md
-## Problem Statement
-
-#Write a Python program that takes two integer inputs from the user and calculates their sum. The program should perform the following tasks:
-#
-#1. Prompt the user to enter the first number.
-#
-#2. Read the input and convert it to an integer.
-#
-#3. Prompt the user to enter the second number.
-#
-#4. Read the input and convert it to an integer.
-#
-#5. Calculate the sum of the two numbers.
-#
-#6. Print the total sum with an appropriate message.
-#
-#The provided solution demonstrates a working implementation of this problem, where the main() function guides the user through the process of entering two numbers and displays their sum.
-
-## Starter Code
-
-
-def main():
-    print("Delete this line and write your code here! :)")
-
-
-# This provided line is required at the end of
-# Python file to call the main() function.
-if __name__ == '__main__':
-    main()
-
-
-## Solution
-
-
-"""
-Program: add2numbers
---------------------
-Another python program to get some practice with
-variables.  This program asks the user for two
-integers and prints their sum.
-"""
-
-
 def main():
     print("This program adds two numbers.")
     num1 : str = input("Enter first number: ")
@@ -52,7 +8,6 @@ def main():
     print("The total is " + str(total) + ".")
 
 
-# This provided line is required at the end of
-# Python file to call the main() function.
+# Python file to call the main() func
 if __name__ == '__main__':
     main()
